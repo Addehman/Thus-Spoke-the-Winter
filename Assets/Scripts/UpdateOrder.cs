87b0313521f23111408ecb92ab843d4bffbf6bc9
Adam Forsell <adafor2001@skola.goteborg.se>
2020-12-08 00:44:26 +0100

    Trees in Order, player character, basics

---
+++ Assets/Scripts/UpdateOrder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateOrder : MonoBehaviour
+{
+	private int orderIncrease = 100, newOrder = 0;
+
+	private SpriteRenderer spriteRend;
+
+	private void Start() {
+		spriteRend = GetComponentInChildren<SpriteRenderer>();
+	}
+
+	private void Update() {
+
+		newOrder = (int)(transform.position.y * orderIncrease) * -1;
+		spriteRend.sortingOrder = newOrder;
+
+		if (spriteRend.sortingOrder == newOrder)
+			Destroy(this);
+	}
+}
