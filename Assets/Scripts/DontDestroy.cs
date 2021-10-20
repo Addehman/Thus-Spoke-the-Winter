3b177f4c98365751d874e8de9c08e47ae1b55f94
Adam Forsell <adamforsell2@hotmail.com>
2021-10-20 12:17:53 +0200

    Fixed errors when switching scenes - now everything that needs to stay loaded needs to be within the "DontDestroyGroup"

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -1,40 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
-    private void Awake()
-    {
+	private void Awake()
+	{
+		GameObject[] objs = GameObject.FindGameObjectsWithTag("DontDestroy");
+		if (objs.Length > 1)
+			Destroy(gameObject);
 
-        GameObject[] worldGroups = GameObject.FindGameObjectsWithTag("WorldGroup");
-
-        if (worldGroups != null)
-        {
-            if (worldGroups.Length > 1)
-            {
-                Destroy(this.gameObject);
-            }
-            DontDestroyOnLoad(this.gameObject);
-            return;
-        }
-
-        var camera = GetComponent<Camera>();
-
-        if (camera != null)
-        {
-            Camera[] cameras = FindObjectsOfType<Camera>();
-
-            if (cameras.Length > 1)
-            {
-                Destroy(this.gameObject);
-            }
-            DontDestroyOnLoad(this.gameObject);
-            return;
-        }
+		DontDestroyOnLoad(gameObject);
 
 
-        /*var player = GetComponent<PlayerController>();
+		/*GameObject[] worldGroups = GameObject.FindGameObjectsWithTag("WorldGroup");
+
+		if (worldGroups != null) {
+			if (worldGroups.Length > 1) {
+				Destroy(this.gameObject);
+			}
+			DontDestroyOnLoad(this.gameObject);
+			return;
+		}
+
+		var camera = GetComponent<Camera>();
+
+		if (camera != null) {
+			Camera[] cameras = FindObjectsOfType<Camera>();
+
+			if (cameras.Length > 1) {
+				Destroy(this.gameObject);
+			}
+			DontDestroyOnLoad(this.gameObject);
+			return;
+		}
+
+
+		*//*var player = GetComponent<PlayerController>();
 
         if (player != null)
         {
@@ -58,20 +59,18 @@ public class DontDestroy : MonoBehaviour
             }
             DontDestroyOnLoad(this.gameObject);
             return;
-        }*/
+        }*//*
 
-        var forestController = GetComponent<ForestController>();
+		var forestController = GetComponent<ForestController>();
 
-        if (forestController != null)
-        {
-            ForestController[] forestControllers = FindObjectsOfType<ForestController>();
+		if (forestController != null) {
+			ForestController[] forestControllers = FindObjectsOfType<ForestController>();
 
-            if (forestControllers.Length > 1)
-            {
-                Destroy(this.gameObject);
-            }
-            DontDestroyOnLoad(this.gameObject);
-            return;
-        }
-    }
+			if (forestControllers.Length > 1) {
+				Destroy(this.gameObject);
+			}
+			DontDestroyOnLoad(this.gameObject);
+			return;
+		}*/
+	}
 }
