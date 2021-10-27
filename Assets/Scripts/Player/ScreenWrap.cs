11a81dd4952249735aad95c0845b11957d9016e1
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2021-10-27 15:35:31 +0200

    Somewhat cleancode. Made a function for the ViewportPointToWorld conversion

--- Assets/Scripts/Player/ScreenWrap.cs
+++ Assets/Scripts/Player/ScreenWrap.cs
@@ -3,104 +3,96 @@ using UnityEngine;
 
 public class ScreenWrap : MonoBehaviour
 {
-	public event Action<string> PlayerTraveling;
+    public event Action<string> PlayerTraveling;
 
-	private Transform _transform;
-	private Camera _cam;
+    private Transform _transform;
+    private Camera _cam;
 
-	[SerializeField]
-	private Vector3 playerViewPortPos;
-	[SerializeField] private bool hasEnergy = true;
-	[SerializeField] private SeedGenerator _seedGenerator;
+    [SerializeField]
+    private Vector3 playerViewPortPos;
+    [SerializeField] private bool hasEnergy = true;
+    [SerializeField] private SeedGenerator _seedGenerator;
 
 
-	void Start()
-	{
-		_transform = transform;
-		_cam = Camera.main;
+    void Start()
+    {
+        _transform = transform;
+        _cam = Camera.main;
 
-		EnergyController.Instance.OutOfEnergy += ToggleHasEnergyBool;
-	}
+        EnergyController.Instance.OutOfEnergy += ToggleHasEnergyBool;
+    }
 
-	void Update()
-	{
-		Wrap();
-	}
+    void Update()
+    {
+        Wrap();
+    }
 
-	private void Wrap()
-	{
-		playerViewPortPos = _cam.WorldToViewportPoint(_transform.position);
+    private void Wrap()
+    {
+        playerViewPortPos = _cam.WorldToViewportPoint(_transform.position);
 
-		/*print(playerViewPortPos);*/
+        if (playerViewPortPos.x > 1f)
+        {
+            if (!hasEnergy && _seedGenerator.worldGrid[_seedGenerator.position.x + 1, _seedGenerator.position.y] == 0)
+            {
+                ConvertPosFromViewPortToWorldPoint(false, true, 1f, "east");
+                return;
+            }
+            ConvertPosFromViewPortToWorldPoint(true, true, 0f, "east");
+        }
+        else if (playerViewPortPos.x < 0f)
+        {
+            if (!hasEnergy && _seedGenerator.worldGrid[_seedGenerator.position.x - 1, _seedGenerator.position.y] == 0)
+            {
+                ConvertPosFromViewPortToWorldPoint(false, true, 0f, "west");
+                return;
+            }
 
-		if (playerViewPortPos.x > 1f) {
-			if (!hasEnergy && _seedGenerator.worldGrid[_seedGenerator.position.x + 1, _seedGenerator.position.y] == 0) {
-				playerViewPortPos.x = 1f;
-				Vector3 notNewPos = _cam.ViewportToWorldPoint(playerViewPortPos);
-				notNewPos.y = _transform.position.y;
-				_transform.position = notNewPos;
-				return;
-			}
-			playerViewPortPos.x = 0f;
+            ConvertPosFromViewPortToWorldPoint(true, true, 1f, "west");
+        }
+        else if (playerViewPortPos.y > 1f)
+        {
+            if (!hasEnergy && _seedGenerator.worldGrid[_seedGenerator.position.x, _seedGenerator.position.y + 1] == 0)
+            {
+                ConvertPosFromViewPortToWorldPoint(false, false, 1f, "north");
+                return;
+            }
+            ConvertPosFromViewPortToWorldPoint(true, false, -1f, "north");
+        }
+        else if (playerViewPortPos.y < 0f)
+        {
+            if (!hasEnergy && _seedGenerator.worldGrid[_seedGenerator.position.x, _seedGenerator.position.y - 1] == 0)
+            {
+                ConvertPosFromViewPortToWorldPoint(false, false, 0f, "south");
+                return;
+            }
 
-			Vector3 newPos = _cam.ViewportToWorldPoint(playerViewPortPos);
-			newPos.y = _transform.position.y;
-			_transform.position = newPos;
+            ConvertPosFromViewPortToWorldPoint(true, false, 2f, "south");
+        }
+    }
 
-			PlayerTraveling?.Invoke("east");
-		}
-		else if (playerViewPortPos.x < 0f) {
-			if (!hasEnergy && _seedGenerator.worldGrid[_seedGenerator.position.x - 1, _seedGenerator.position.y] == 0) {
-				playerViewPortPos.x = 0f;
-				Vector3 notNewPos = _cam.ViewportToWorldPoint(playerViewPortPos);
-				notNewPos.y = _transform.position.y;
-				_transform.position = notNewPos;
-				return;
-			}
-			playerViewPortPos.x = 1f;
+    private void ConvertPosFromViewPortToWorldPoint(bool shouldMove, bool changeX, float value, string latitude)
+    {
+        if (changeX)
+        {
+            playerViewPortPos.x = value;
+        }
+        else
+        {
+            playerViewPortPos.y = value;
+        }
 
-			Vector3 newPos = _cam.ViewportToWorldPoint(playerViewPortPos);
-			newPos.y = _transform.position.y;
-			_transform.position = newPos;
+        Vector3 newPos = _cam.ViewportToWorldPoint(playerViewPortPos);
+        newPos.y = _transform.position.y;
+        _transform.position = newPos;
 
-			PlayerTraveling?.Invoke("west");
-		}
-		else if (playerViewPortPos.y > 1f) {
-			if (!hasEnergy && _seedGenerator.worldGrid[_seedGenerator.position.x, _seedGenerator.position.y + 1] == 0) {
-				playerViewPortPos.y = 1f;
-				Vector3 notNewPos = _cam.ViewportToWorldPoint(playerViewPortPos);
-				notNewPos.y = _transform.position.y;
-				_transform.position = notNewPos;
-				return;
-			}
-			playerViewPortPos.y = -1f;
+        if (!shouldMove) return;
 
-			Vector3 newPos = _cam.ViewportToWorldPoint(playerViewPortPos);
-			newPos.y = _transform.position.y;
-			_transform.position = newPos;
+        PlayerTraveling?.Invoke(latitude);
+    }
 
-			PlayerTraveling?.Invoke("north");
-		}
-		else if (playerViewPortPos.y < 0f) {
-			if (!hasEnergy && _seedGenerator.worldGrid[_seedGenerator.position.x, _seedGenerator.position.y - 1] == 0) {
-				playerViewPortPos.y = 0f;
-				Vector3 notNewPos = _cam.ViewportToWorldPoint(playerViewPortPos);
-				notNewPos.y = _transform.position.y;
-				_transform.position = notNewPos;
-				return;
-			}
-			playerViewPortPos.y = 2f;
-
-			Vector3 newPos = _cam.ViewportToWorldPoint(playerViewPortPos);
-			newPos.y = _transform.position.y;
-			_transform.position = newPos;
-
-			PlayerTraveling?.Invoke("south");
-		}
-	}
-
-	private void ToggleHasEnergyBool()
-	{
-		hasEnergy = !hasEnergy;
-	}
+    private void ToggleHasEnergyBool()
+    {
+        hasEnergy = !hasEnergy;
+    }
 }
