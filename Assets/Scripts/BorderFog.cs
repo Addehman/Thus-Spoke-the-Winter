0380c5e1ed1761603a93f0758bd32248e3122fbb
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2021-10-28 15:35:22 +0200

    Fixed BorderFog to spawn fog directly when energy is depleted. Also minor clean code.

--- Assets/Scripts/BorderFog.cs
+++ Assets/Scripts/BorderFog.cs
@@ -8,6 +8,7 @@ public class BorderFog : MonoBehaviour
     [SerializeField] private SeedGenerator _seedGenerator;
     [SerializeField] private GameObject _northFog, _eastFog, _southFog, _westFog;
     private bool _fogActive = false;
+    private bool _northActive, _eastActive, _southActive, _westActive;
 
 
     void Start()
@@ -19,17 +20,29 @@ public class BorderFog : MonoBehaviour
     void ActivateFog()
     {
         _fogActive = true;
+
+        SpawnFog();
     }
 
     void UpdateFog(bool north, bool east, bool south, bool west)
     {
+        _northActive = north;
+        _eastActive = east;
+        _southActive = south;
+        _westActive = west;
+
         if (_fogActive)
         {
-            _northFog.SetActive(north);
-            _eastFog.SetActive(east);
-            _southFog.SetActive(south);
-            _westFog.SetActive(west);
+            SpawnFog();
         }
+    }
+
+    void SpawnFog()
+    {
+        _northFog.SetActive(_northActive);
+        _eastFog.SetActive(_eastActive);
+        _southFog.SetActive(_southActive);
+        _westFog.SetActive(_westActive);
     }
 
     private void OnDestroy()
