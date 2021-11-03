209a106be14e8abe91a79bb510993ef8e4d91d8e
Adam Forsell <adamforsell2@hotmail.com>
2021-11-03 19:43:15 +0100

    fixed bugs: no more looking into house from outside wall; now color of ground is changed according to the season also at start().

--- Assets/Scripts/GameManager/SeasonController.cs
+++ Assets/Scripts/GameManager/SeasonController.cs
@@ -7,44 +7,45 @@ public enum Seasons { earlySpring, lateSpring, earlySummer, lateSummer, earlyFal
 
 public class SeasonController : MonoBehaviour
 {
-    private static SeasonController _instance;
-    public static SeasonController Instance { get { return _instance; } }
+	private static SeasonController _instance;
+	public static SeasonController Instance { get { return _instance; } }
 
-    [SerializeField] private Seasons currentSeason = Seasons.earlySpring;
+	[SerializeField] private Seasons currentSeason = Seasons.earlySpring;
 
-    public event Action GameOver;
-    public event Action<Seasons> UpdateSeason;
+	public event Action GameOver;
+	public event Action<Seasons> UpdateSeason;
 
-    private void Awake()
-    {
-        if (_instance != null && _instance != this)
-            Destroy(this);
-        else
-            _instance = this;
-    }
 
-    private void Start()
-    {
-        EnergyController.Instance.EnergyRestored += IncrementSeason;
-        UpdateSeason?.Invoke(currentSeason);
-    }
+	private void Awake()
+	{
+		if (_instance != null && _instance != this)
+			Destroy(this);
+		else
+			_instance = this;
+	}
 
-    private void IncrementSeason()
-    {
-        if (currentSeason != Seasons.winter)
-        {
-            currentSeason++;
-            UpdateSeason?.Invoke(currentSeason);
-        }
-        else
-        {
-            GameOver?.Invoke();
-        }
-    }
+	private void Start()
+	{
+		EnergyController.Instance.EnergyRestored += IncrementSeason;
+		UpdateSeason?.Invoke(currentSeason);
+	}
 
-    private void OnDestroy()
-    {
-        EnergyController.Instance.EnergyRestored -= IncrementSeason;
-    }
+	private void IncrementSeason()
+	{
+		if (currentSeason != Seasons.winter)
+		{
+			currentSeason++;
+			UpdateSeason?.Invoke(currentSeason);
+		}
+		else
+		{
+			GameOver?.Invoke();
+		}
+	}
+
+	private void OnDestroy()
+	{
+		EnergyController.Instance.EnergyRestored -= IncrementSeason;
+	}
 }
 
--- Assets/Scripts/GroundBehaviour.cs
+++ Assets/Scripts/GroundBehaviour.cs
@@ -5,42 +5,43 @@ using UnityEngine;
 
 public class GroundBehaviour : MonoBehaviour
 {
-    [SerializeField] private Material material;
-    [SerializeField] private Color spring, summer, fall, winter;
-    void Start()
-    {
-        SeasonController.Instance.UpdateSeason += UpdateGFX;
-    }
+	[SerializeField] private Material material;
+	[SerializeField] private Color spring, summer, fall, winter;
+	void Start()
+	{
+		SeasonController.Instance.UpdateSeason += UpdateGFX;
+		UpdateGFX();
+	}
 
-    private void UpdateGFX(Seasons currentSeason)
-    {
-        switch (currentSeason)
-        {
-            case Seasons.earlySpring:
-                material.color = spring;
-                break;
-            case Seasons.lateSpring:
-                break;
-            case Seasons.earlySummer:
-                material.color = summer;
-                break;
-            case Seasons.lateSummer:
-                break;
-            case Seasons.earlyFall:
-                material.color = fall;
-                break;
-            case Seasons.lateFall:
-                break;
-            case Seasons.winter:
-                material.color = winter;
-                break;
-            default:
-                break;
-        }
-    }
+	private void UpdateGFX(Seasons currentSeason = Seasons.earlySpring)
+	{
+		switch (currentSeason)
+		{
+			case Seasons.earlySpring:
+				material.color = spring;
+				break;
+			case Seasons.lateSpring:
+				break;
+			case Seasons.earlySummer:
+				material.color = summer;
+				break;
+			case Seasons.lateSummer:
+				break;
+			case Seasons.earlyFall:
+				material.color = fall;
+				break;
+			case Seasons.lateFall:
+				break;
+			case Seasons.winter:
+				material.color = winter;
+				break;
+			default:
+				break;
+		}
+	}
 
-    private void OnDestroy()
-    {
-        SeasonController.Instance.UpdateSeason -= UpdateGFX;
-    }
+	private void OnDestroy()
+	{
+		SeasonController.Instance.UpdateSeason -= UpdateGFX;
+	}
 }
